a3decf4c6e1b1465c9af82f43496082d1840e644
aozp73 <aozp73@naver.com>
2023-12-20 20:33:53 +0900

    useState()

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Sub from './Sub';
 
 function App() {
-  let li = [1, 2, 3];
+  console.log('App 실행');
+
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '장보고' },
+    { id: 4, name: '코스' },
+  ];
+
+  const [users, setUsers] = useState(sample);
+
+  const download = () => {
+    setUsers([...sample, { id: 5, name: '조자룡' }]);
+  };
+
   return (
     <div>
-      <div>
-        {li.map((n) => (
-          <h1>{n}</h1>
-        ))}
-      </div>
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <h1>
+          {u.id}. {u.name}
+        </h1>
+      ))}
     </div>
   );
 }
