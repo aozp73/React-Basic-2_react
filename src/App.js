e0dd3dc33bd461c4dfe8d58accd1b4012331f4f0
aozp73 <aozp73@naver.com>
2023-12-21 16:53:30 +0900

    components styled

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { createRef, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const myRef = useRef(null);
-
-  const [list, setList] = useState([
-    { id: 1, name: '길동' },
-    { id: 2, name: '꺽정' },
-  ]);
-  const myRefs = Array.from({ length: list.length }).map(() => useRef());
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          console.log(myRef.current);
-          // myRef.current.style.backgroundColor = 'red';
-          myRefs[1].current.style.backgroundColor = 'red';
-        }}
-      >
-        색 변경
-      </button>
-      <div ref={myRef}>박스</div>
-      {list.map((user, index) => (
-        <h1 ref={myRefs[index]}>{user.name}</h1>
-      ))}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
