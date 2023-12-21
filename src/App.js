6fdf30f7f79a6c496bf2e103be92638dbd993f87
aozp73 <aozp73@naver.com>
2023-12-21 15:52:46 +0900

    useRef()

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import { useMemo, useState } from 'react';
+import { createRef, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [list, setList] = useState([1, 2, 3, 4]);
-  const [str, setStr] = useState('합계');
+  const myRef = useRef(null);
 
-  const getAddResult = () => {
-    let sum = 0;
-    list.forEach((i) => (sum += i));
-    console.log('sum', sum);
-    console.log('str', str);
-    return sum;
-  };
-  const addResult = useMemo(() => getAddResult(), [list]);
+  const [list, setList] = useState([
+    { id: 1, name: '길동' },
+    { id: 2, name: '꺽정' },
+  ]);
+  const myRefs = Array.from({ length: list.length }).map(() => useRef());
 
   return (
     <div>
-      <button onClick={() => setStr('안녕')}>문자 변경</button>
-      <button onClick={() => setList([...list, 10])}>리스트값 추가</button>
-      <div>
-        {list.map((i) => (
-          <h2>{i}</h2>
-        ))}
-      </div>
-      <div>
-        {str}: {addResult}
-      </div>
+      <button
+        onClick={() => {
+          console.log(myRef.current);
+          // myRef.current.style.backgroundColor = 'red';
+          myRefs[1].current.style.backgroundColor = 'red';
+        }}
+      >
+        색 변경
+      </button>
+      <div ref={myRef}>박스</div>
+      {list.map((user, index) => (
+        <h1 ref={myRefs[index]}>{user.name}</h1>
+      ))}
     </div>
   );
 }
