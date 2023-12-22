7ea16e2628d783cf137ca192988b7b77b40fc0fe
aozp73 <aozp73@naver.com>
2023-12-22 13:59:47 +0900

    styled component 스타일 상속

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,12 +5,17 @@ const StyledDeleteButton = styled.button`
   color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
 `;
 
+const StyledAddButton = styled(StyledDeleteButton)`
+  background-color: yellowgreen;
+`;
+
 const Home = (props) => {
   const { boards, setBoards, user } = props;
 
   return (
     <div>
       <h1>홈페이지입니다.</h1>
+      <StyledAddButton user={user}>더하기</StyledAddButton>
       <StyledDeleteButton user={user} onClick={() => setBoards([])}>
         전체삭제
       </StyledDeleteButton>
