0444526b43747628d66f9261edf0fc412d207b16
aozp73 <aozp73@naver.com>
2023-12-22 16:10:47 +0900

    BootStrap 연동

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import styled from 'styled-components';
 
-const StyledHeaderDiv = styled.div`
-  border: 1px solid black;
-  height: 100px;
-  background-color: ${(props) => props.backgroudColor};
-`;
+// const StyledHeaderDiv = styled.div`
+//   border: 1px solid black;
+//   height: 100px;
+//   background-color: ${(props) => props.backgroudColor};
+// `;
 
-const StyledHeaderLink = styled(Link)`
-  color: red;
-`;
+// const StyledHeaderLink = styled(Link)`
+//   color: red;
+// `;
 
 const Header = () => {
   return (
     <div>
-      <StyledHeaderDiv backgroudColor="lightgray">
+      {/* <StyledHeaderDiv backgroudColor="lightgray">
         <ul>
           <li>
             <StyledHeaderLink to="/">홈</StyledHeaderLink>
@@ -23,7 +24,21 @@ const Header = () => {
             <StyledHeaderLink to="/login/10">로그인</StyledHeaderLink>
           </li>
         </ul>
-      </StyledHeaderDiv>
+      </StyledHeaderDiv> */}
+
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/login/10" className="nav-link">
+              login
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -15,6 +16,7 @@ const Home = (props) => {
   return (
     <div>
       <h1>홈페이지입니다.</h1>
+      <Button variant="primary">Primary</Button>{' '}
       <StyledAddButton user={user}>더하기</StyledAddButton>
       <StyledDeleteButton user={user} onClick={() => setBoards([])}>
         전체삭제
